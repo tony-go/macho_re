e3baafe254a047952444aad47d49d6eaff13ef08
Tony Gorez <gorez.tony@gmail.com>
2024-11-27 09:56:55 +0100

    refactor: do safe version and dylib name copy
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- quickmacho.c
+++ quickmacho.c
@@ -6,6 +6,7 @@
 #include <string.h>
 
 #define VERSION_STR_SIZE 16
+#define NAME_STR_SIZE 256
 
 void print_usage(const char *program_name)
 {
@@ -16,15 +17,24 @@ void print_usage(const char *program_name)
 // The version is a 32-bit integer in the format 0xMMmmPPPP, where MM is the
 // major version, mm is the minor version, and PPPP is the patch version.
 // We need to extract these values and print them in the format MM.mm.PPPP.
-void parse_dylib_version(uint32_t version, char *output_version_str, size_t output_version_str_size)
+bool parse_dylib_version(struct dylib_command *cmd, char *output_version_str, size_t output_version_str_size)
 {
+  uint32_t version = cmd->dylib.current_version;
   uint32_t major = (version >> 24) & 0xFF;
   uint32_t minor = (version >> 16) & 0xFF;
   uint32_t patch = version & 0xFF;
   char *version_str = malloc(10);
   // return a string in the format MM.mm.PPPP
-  snprintf(output_version_str, output_version_str_size, "%u.%u.%u", major,
-           minor, patch);
+  size_t written = snprintf(output_version_str, output_version_str_size, "%u.%u.%u", major,
+                            minor, patch);
+  return written >= output_version_str_size;
+}
+
+bool parse_dylib_name(struct dylib_command *cmd, char *output_name_str, size_t output_name_str_size)
+{
+  const char *name = (char *)cmd + cmd->dylib.name.offset;
+  size_t written = snprintf(output_name_str, output_name_str_size, "%s", name);
+  return written >= output_name_str_size;
 }
 
 void parse_load_commands(uint8_t *buffer, uint32_t ncmds)
@@ -56,11 +66,18 @@ void parse_load_commands(uint8_t *buffer, uint32_t ncmds)
     case LC_LAZY_LOAD_DYLIB:
     {
       struct dylib_command *dylib_cmd = (struct dylib_command *)cmd;
-      const char *name = (char *)cmd + dylib_cmd->dylib.name.offset;
-      uint32_t version = dylib_cmd->dylib.current_version;
-      printf("Load dylib: %s - ", name);
+
+      char name_str[NAME_STR_SIZE];
+      bool is_name_truncated = parse_dylib_name(dylib_cmd, name_str, NAME_STR_SIZE);
+      printf("Load dylib: %s ", name_str);
+      if (is_name_truncated)
+      {
+        printf("(truncated) ");
+      }
+      printf("- ");
+
       char version_str[VERSION_STR_SIZE];
-      parse_dylib_version(version, version_str, VERSION_STR_SIZE);
+      parse_dylib_version(dylib_cmd, version_str, VERSION_STR_SIZE);
       printf("version: %s\n", version_str);
       break;
     }
