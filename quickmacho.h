c5f14f3b54f760c1c8a3016b3cae5a6b797a42cc
Tony Gorez <gorez.tony@gmail.com>
2024-11-28 10:41:50 +0100

    feat: add is_fat_header api
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- quickmacho.h
+++ quickmacho.h
@@ -3,10 +3,15 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 
 // Parse a Mach-O binary buffer
 void parse_mach_o(uint8_t *buffer);
 
 // Parse a Fat binary buffer
 void parse_fat(uint8_t *buffer, size_t size);
+
+// Check if a buffer is a Fat binary
+bool is_fat_header(uint8_t *buffer);
+
 #endif
