1632a4498d7575c115ace7f7dfd7010c69c87164
Tony Gorez <gorez.tony@gmail.com>
2024-12-12 12:03:12 +0100

    test(lib): basic parse method (#17)
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/test_main.cc
+++ test/test_main.cc
@@ -1,7 +1,38 @@
-#include <gtest/gtest.h>
-
 extern "C" {
 #include "../lib/libmachore.h"
+}
+
+#include <gtest/gtest.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+static void read_file_to_buffer(const char *filename, uint8_t **buffer,
+                                size_t *size) {
+  FILE *file = fopen(filename, "rb");
+  if (!file) {
+    printf("Error: Cannot open file '%s'\n", filename);
+    return;
+  }
+
+  fseek(file, 0, SEEK_END);
+  *size = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  *buffer = (uint8_t *)malloc(*size);
+  if (!*buffer) {
+    printf("Error: Memory allocation failed\n");
+    fclose(file);
+    return;
+  }
+
+  if (fread(*buffer, 1, *size, file) != *size) {
+    printf("Error: Failed to read file\n");
+    free(*buffer);
+    fclose(file);
+    return;
+  }
+  fclose(file);
 }
 
 TEST(libmachore, create_analysis) {
@@ -27,4 +58,22 @@ TEST(libmachore, clean_analysis) {
   EXPECT_EQ(analysis.arch_analyses, nullptr);
   EXPECT_EQ(analysis.num_arch_analyses, 0);
   EXPECT_EQ(analysis.is_fat, false);
+}
+
+TEST(libmachore, parse_macho) {
+  struct analysis analysis;
+  create_analysis(&analysis);
+
+  const char *filename = "/bin/ls";
+  uint8_t *buffer = nullptr;
+  size_t buffer_size = 0;
+  read_file_to_buffer(filename, &buffer, &buffer_size);
+
+  parse_macho(&analysis, buffer, buffer_size);
+
+  EXPECT_EQ(analysis.num_arch_analyses, 2);
+  EXPECT_EQ(analysis.is_fat, true);
+
+  free(buffer);
+  clean_analysis(&analysis);
 }
