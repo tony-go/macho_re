df87449434274b51f5f379ad05f4803d1d051565
Tony Gorez <gorez.tony@gmail.com>
2024-12-12 11:35:32 +0100

    test(lib): test clean (#14)
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/test_main.cc
+++ test/test_main.cc
@@ -14,3 +14,17 @@ TEST(libmachore, create_analysis) {
 
   clean_analysis(&analysis);
 }
+
+TEST(libmachore, clean_analysis) {
+  struct analysis analysis;
+  create_analysis(&analysis);
+  analysis.arch_analyses =
+      (struct arch_analysis *)malloc(sizeof(struct arch_analysis));
+  analysis.num_arch_analyses = 1;
+
+  clean_analysis(&analysis);
+
+  EXPECT_EQ(analysis.arch_analyses, nullptr);
+  EXPECT_EQ(analysis.num_arch_analyses, 0);
+  EXPECT_EQ(analysis.is_fat, false);
+}
